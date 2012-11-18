3e5110c83f33900068e2e8833a9b08b07ec9d07c
Andrew Tribone <atribone@gmail.com>
2012-11-17 18:05:33 -0800

    templates work

--- andrewtribone.py
+++ andrewtribone.py
@@ -1,14 +1,15 @@
 import os
 from flask import Flask
+from flask import render_template
+
+DEBUG = True
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 @app.route('/')
 def home():
-	with open('index.html') as f:
-		return render_template('index.html')
+	return render_template('index.html')
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+	app.run()
